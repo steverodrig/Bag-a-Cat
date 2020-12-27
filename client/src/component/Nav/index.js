ed0197d97204942face9d86a9dcb6dd234a3d4e5
neyneyalldayday <drfrankenstein123@gmail.com>
2020-12-27 13:29:28 -0600

    reverted the profile change

--- client/src/component/Nav/index.js
+++ client/src/component/Nav/index.js
@@ -5,7 +5,7 @@ import LoginButton from "../LoginBtn";
 import LogoutButton from "../LogoutBtn";
 import { useAuth0 } from '@auth0/auth0-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import Profile from "../Profile";
 
 function Navbar() {
     const [click, setClick] = useState(false)
@@ -37,7 +37,8 @@ function Navbar() {
     return (
         <>
         <nav className="navbar">
-            <div className="navbar-container">               
+            <div className="navbar-container">
+                <Profile />
                <Link to="/" className="navbar-logo" onClick={closeMobleMenu}>
                <FontAwesomeIcon icon="cat" />
                   B-A-C                  
@@ -45,9 +46,8 @@ function Navbar() {
                <div className="menu-icon" onClick={handleClick}>
                    <i className={click ? 'fas fa-cat' : 'fas fa-bars'} />
                </div>
-               
                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                   <li className='nav-item'>                    
+                   <li className='nav-item'>
                     <Link to='/' className='nav-links' onClick={closeMobleMenu}>
                         Home
                     </Link>
