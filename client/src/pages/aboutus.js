df7573f58d6a4dcde9c7eb04bff56f54dc847d87
neyneyalldayday <drfrankenstein123@gmail.com>
2020-12-27 11:11:05 -0600

    added a small about us

--- client/src/pages/aboutus.js
+++ client/src/pages/aboutus.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { TweenMax, Power3 } from "gsap";
+import "../component/aboutus.css"
 
 
 
@@ -14,9 +15,15 @@ function Aboutus() {
     
   }, [])
     return (
-        <div className="aboutus">
+        <div className="aboutus-container">
+           <video src="/video/B-A-C.mp4" autoPlay loop muted />
              <h1 ref={el => {Title = el}}>bag a cat</h1>
              <p ref ={el => {textItem = el}}>find your furr demon</p>
+             <p>Bag a cat is a non profit organization.
+             our sole focus is to get fluffy critters into your home
+             no sad sarah mcglothlin music, our cats arent sad
+             happy cats only
+             alothough they still break all your stuff. but hey they are cute</p>
         </div>
     )
 }
