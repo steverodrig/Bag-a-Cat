46248b203ec55440f8124ad4da13585b7e027c46
neyneyalldayday <drfrankenstein123@gmail.com>
2021-01-07 14:06:24 -0600

    styled up a few more items

---
+++ client/src/component/thaBois.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ThaBois() {
+    return (
+        <div>
+          <img src='/images/wp1920612.png' 
+          srcSet='/images/wp1920612.png,       
+                  /images/wp1920612-1000.png,       
+                  /images/wp1920612-1500.png'         
+          alt=''></img>    
+        </div>
+    )
+}
+
+export default ThaBois
