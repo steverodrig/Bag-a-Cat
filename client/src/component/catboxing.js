467dadecd6dafd46b2fc0e605de955406346a137
neyneyalldayday <drfrankenstein123@gmail.com>
2021-01-09 13:07:23 -0600

    cat image

--- client/src/component/catboxing.js
+++ client/src/component/catboxing.js
@@ -4,7 +4,7 @@ import '../component/catboxing.css'
 function Catboxing() {
     return (
         <div className='catboxing'>
-          <img src='/images/catboxing8p11.png' 
+          <img  className='catboxing' src='/images/catboxing8p11.png' 
           srcSet='/images/catboxing8p11.png,       
                   /images/catboxing8p11-1000.png,       
                   /images/catboxing8p11-1500.png'         
