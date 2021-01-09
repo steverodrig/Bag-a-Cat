26b505c012699c991c4d9e083b95bd2f03422ddf
neyneyalldayday <drfrankenstein123@gmail.com>
2021-01-08 19:59:44 -0600

    dead

--- client/src/App.js
+++ client/src/App.js
@@ -19,15 +19,18 @@ import Contact from "./pages/contact"
 import Support from "./pages/support"
 import Locations from "./pages/locations"
 import Investors from "./pages/investors"
+
 import { BrowserRouter as Router, Switch,  Route, } from "react-router-dom";
 import Testimonials from "./pages/testimonials";
+
+
 
 function App() {
   
   return (
     <>
        <Router>
-        <Navbar />
+        <Navbar />     
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/aboutus' component={Aboutus} />
@@ -42,18 +45,21 @@ function App() {
           <Route path='/admin' component={Admin} />
           <Route path='/errorPage'component={ErrorPage} />
           <Route path='/catApp/:id' component={CatApp} />         
-          <Route path='/investors' component={Investors} />
           <Route path='/contact' component={Contact} />
           <Route path='/support' component={Support} />
-          <Route path='/locations' component={Locations} /> 
-          <Route path='/catpost'  component={Catpost} /> 
+          <Route path='/locations' component={Locations} />           
+          <Route path='/catpost'  component={Catpost} />         
+          <Route path='/investors'  component={Investors} />         
           <Route path= '/catmemes' component={() => {window.location.href = "https://imgur.com/search/score?q=cat+memes"; return null}}/>
           <Route path= '/YouTube' component={() => {window.location.href = "https://www.youtube.com/channel/UCNo5PGwGmfnEprEUUb9AGgw"; return null}}/>
           <Route path= '/Cattitude' component={() => {window.location.href = "https://scratch.mit.edu/projects/404898794/fullscreen/"; return null}}/>
-        </Switch>
+      
+          </Switch>
       </Router>
     </>
   );
 }
 
 export default App;
+
+
